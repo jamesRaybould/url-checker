20d09ac9938851d1eff9aa12280b0c086e8f63b8
James Raybould <james.raybould@gmail.com>
2015-08-11 23:29:25 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+#URL checker
+A quick and dirty way of hitting a set of URLs written in Golang
+
+##Usage
+`urlChecker --csvPath=urls.csv`
+
+Where `urls.csv` is a list of valid URLs with a line break after each URL
+
+##Running
+- Make sure that you have `golang` installed (using `brew install go` would be my recommended way on OSX)
+- Change into the directory on the command line
+- run `export GOPATH=$PWD && export GOBIN=$PWD/bin`
+- `go run src/github.com/jamesRaybould/url-checker/urlChecker.go`
+
+##Building
+As above but run `go build src/github.com/jamesRaybould/url-checker/urlChecker.go`
+
+This will give you an executable in the current directory that can be run on *any* osx machine
---
+++ src/github.com/jamesRaybould/url-checker/urlChecker.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+var csvPath string
+
+func init() {
+	flag.StringVar(&csvPath, "csvPath", "urls.csv", "The path of the csv to parse")
+}
+
+func main() {
+	flag.Parse()
+
+	urlsToHit, err := readUrls(csvPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//Create a wait group so we can fire off all the goroutines and wait for them to complete
+	var wg sync.WaitGroup
+
+	for key, val := range urlsToHit {
+		//Fire off as many go routines as we have URLs
+		//pewpewpewpewpewpew
+		wg.Add(1)
+		//index[0] will always contain the one and only element of a valid URL csv
+		go hitUrl(val[0], &wg)
+
+		//Some *super* naive throttling
+		//every 10 urls pause for a second before adding another 10 urls
+		if key > 0 && key%10 == 0 {
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	//Sit here until all the goroutines are completed, then exit nicely
+	wg.Wait()
+}
+
+//Creates an array of the urls that are present in the CSV
+func readUrls(path string) ([][]string, error) {
+	csvfile, err := os.Open(path)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer csvfile.Close()
+
+	reader := csv.NewReader(csvfile)
+	reader.FieldsPerRecord = 1
+
+	CSVdata, err := reader.ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return CSVdata, nil
+}
+
+//Hit the chosen URL using a GET request and signal its completion using the waitGroup
+//Prints out the URL and the Status code to the command line
+func hitUrl(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Printf("Url : %s Status: %s\n", url, resp.Status)
+}
